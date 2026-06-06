aa57c35de789c37a0a082988d0be14e53d7ed6aa
ogbonna Chigozie Paschal <bobpaschal42@gmail.com>
2026-06-06 22:03:23 +0100

    feat: User Interface updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
  import AIFeatureds from "@/components/AIFeatureds"
-import BrowseCategory from "@/components/BrowseCategory"
 import Explore from "@/components/Explore"
 import HeroSection from "@/components/HeroSection"
 import HomeDwellWorks from "@/components/HowDewllWorks"
-import Testimonies from "@/components/Testimonials"
+{/* import Testimonies from "@/components/Testimonials"  */}
 import TrustSection from "@/components/TrustSection"
 export default function Home() {
   return (
